45aa7fdd9291e1e281b46e5d40c9e355b7593f5d
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2024-09-25 22:48:20 +0330

    rendering products form api

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { useProducts } from "../context/ProductProvider";
-
+import styles from "./ProductsPage.module.css";
 function ProductsPage() {
   const products = useProducts();
   console.log(products);
-  return <div>ProductsPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.products}>
+        {!products.length && <p>loading</p>}
+        {products.map((product) => (
+          <p>{product.title}</p>
+        ))}
+      </div>
+      <div>sidebar</div>
+    </div>
+  );
 }
 
 export default ProductsPage;
