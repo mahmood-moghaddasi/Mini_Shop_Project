723184b25377d43e9221751d1dd416446f852d71
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2024-09-25 23:22:55 +0330

    adding Loading from react-loader-spinner librarry

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { useProducts } from "../context/ProductProvider";
 import styles from "./ProductsPage.module.css";
 import Card from "../components/Card";
+import Loader from "../components/Loader";
 function ProductsPage() {
   const products = useProducts();
   console.log(products);
   return (
     <div className={styles.container}>
       <div className={styles.products}>
-        {!products.length && <p>loading</p>}
+        {!products.length && <Loader />}
         {products.map((product) => (
           <Card key={product.id} data={product} />
         ))}
